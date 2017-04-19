c078013d3d12e8e43c8fe9996526cad0fc0f1b40
Andrew Gurung <andrewgurung@users.noreply.github.com>
2017-04-19 14:42:08 -0400

    Fix grid component import

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-
+    <div id="details">
+      <h1>Tic Tac Toe</h1>
+    </div>
+    <grid></grid>
   </div>
 </template>
 
 <script>
+import Grid from './components/Grid.vue'
+
 export default {
+  components: { Grid },
   name: 'app',
   data () {
     return {
