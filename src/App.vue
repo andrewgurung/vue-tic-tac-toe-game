0e64301c237ce20a72cac59e010de87b2ba56f01
Andrew Gurung <andrewgurung@users.noreply.github.com>
2017-04-20 15:47:20 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app">
-    <div id="details">
-      <h1>Tic Tac Toe</h1>
+  <div>
+    <div class="scoreBoard">
+    <span>O has {{ wins.O }} wins</span>
+    <h2>Score Board</h2>
+    <span>X has {{ wins.X }} wins</span>
     </div>
-    <grid></grid>
+    <div id="app">
+      <div id="details">
+        <h1>Tic Tac Toe</h1>
+        <h2>Match #{{ matches + 1 }}</h2>
+      </div>
+      <grid></grid>
+      <button class="restart" @click="restart">Restart</button>
+    </div>
   </div>
 </template>
 
@@ -22,8 +31,22 @@ export default {
       }
 
     }
+  },
+
+  methods: {
+    restart () {
+      Event.$emit('clearCell')
+      Event.$emit('gridReset')
+      this.matches++
+    }
+  },
+
+  created() {
+    Event.$on('win', winner => this.wins[winner]++);
   }
 }
+
+
 </script>
 
 <style>
